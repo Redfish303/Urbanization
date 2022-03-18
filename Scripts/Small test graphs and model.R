ad7b399a93658d47de8f40502b4222427420cea3
mbelitz <mbelitz@ufl.edu>
2022-03-18 11:53:17 -0400

    updated modeling procedure to only look at interactions b/w traits and urbanization

--- Scripts/Small test graphs and model.R
+++ Scripts/Small test graphs and model.R
@@ -5,6 +5,7 @@ library(stringr)
 library(ggplot2)
 library(lmerTest)
 library(sjPlot)
+library(MuMIn)
 ### script to analyze effect of urbanization on overall abundace by species
 
 #read in abundance data
@@ -26,7 +27,9 @@ cm_long_urbanization <- left_join(cm_long, urb)
 ## combine with trait data
 traits <- read.csv("data/TraitData.csv")
 cm_long_urbanization <- left_join(cm_long_urbanization, traits, 
-                                  by = c("scientific_name" = "Species"))
+                                  by = c("scientific_name" = "Species")) %>% 
+      mutate(Dev_1 = asin(sqrt(Dev_1)), # Attempt to make proportion developed into a more 
+       Dev_10 = asin(sqrt(Dev_10)))     # normally distributed data
 
 #plot all species abundance by urbanization amount with colors and slopes per spp.
 ggplot(cm_long_urbanization, mapping = aes(x = Dev_1, y = abundance, color = scientific_name)) +
@@ -48,11 +51,10 @@ ggplot(cm_long_urbanization, mapping = aes(x = Dev_10, y = abundance, color = sc
     facet_wrap(~scientific_name)
 
 mdf <- cm_long_urbanization %>% 
-    na.omit()
+    na.omit() 
 
 ## quick test to see if abundance of selected species is impacted by urbanization
 model <- lmer(formula = abundance ~ Dev_1 + 
-                  LHSCategory + VoltinismCategory + LarvalHabitatCategory + BodySize +
                   LHSCategory:Dev_1 + VoltinismCategory:Dev_1 + 
                   LarvalHabitatCategory:Dev_1 + BodySize:Dev_1 +
                   (1 | scientific_name), na.action = "na.fail",
@@ -61,21 +63,15 @@ model <- lmer(formula = abundance ~ Dev_1 +
 summary(model)
 
 dd <- MuMIn::dredge(model)
-s <- step(model)
-
 top_model <- get.models(dd, subset = 1)[[1]]
 car::vif(top_model)
 summary(top_model)
 
-sjPlot::plot_model(top_model, type = "pred")
-sjPlot::plot_model(top_model, terms = "BodySize", type = "pred")
-sjPlot::plot_model(top_model, terms = "LarvalHabitatCategory", type = "pred")
-sjPlot::plot_model(top_model, terms = c("Dev_1","LarvalHabitatCategory"), type = "pred")
-sjPlot::plot_model(top_model, terms = c("Dev_1","VoltinismCategory"), type = "pred")
+sjPlot::plot_model(top_model, terms = c("Dev_1","LHSCategory"), type = "pred", ci.lvl = NA)
+sjPlot::plot_model(top_model, terms = c("Dev_1","VoltinismCategory"), type = "pred", ci.lvl = NA)
 
 ## model this at a 10 km scale for imprevious surface
 model2 <- lmer(formula = abundance ~ Dev_10 + 
-                  LHSCategory + VoltinismCategory + LarvalHabitatCategory + BodySize +
                   LHSCategory:Dev_10 + VoltinismCategory:Dev_10 + 
                   LarvalHabitatCategory:Dev_10 + BodySize:Dev_10 +
                   (1 | scientific_name), na.action = "na.fail",
@@ -89,17 +85,19 @@ top_model2 <- get.models(dd2, subset = 1)[[1]]
 car::vif(top_model2)
 summary(top_model2)
 
+sjPlot::plot_model(top_model2, terms = c("Dev_10","VoltinismCategory"), type = "pred", ci.lvl = NA)
+sjPlot::plot_model(top_model2, terms = c("Dev_10","LHSCategory"), type = "pred", ci.lvl = NA)
+
+
 ## look into AICs of 1km and 10km
 AICc(top_model, top_model2) # our top top model is top model 2
 
 top_model <- top_model2
 summary(top_model2)
 
-sjPlot::plot_model(top_model, terms = "Dev_10", type = "pred")
-sjPlot::plot_model(top_model, terms = "BodySize", type = "pred")
-sjPlot::plot_model(top_model, terms = "LarvalHabitatCategory", type = "pred")
-sjPlot::plot_model(top_model, terms = c("Dev_10","LarvalHabitatCategory"), type = "pred")
-sjPlot::plot_model(top_model, terms = c("Dev_10","VoltinismCategory"), type = "pred")
+sjPlot::plot_model(top_model2, terms = c("Dev_10","VoltinismCategory"), type = "pred", ci.lvl = NA)
+sjPlot::plot_model(top_model2, terms = c("Dev_10","LHSCategory"), type = "pred", ci.lvl = NA)
+
 
 ### test for richness
 rich <- cm_long_urbanization %>% 
@@ -122,10 +120,20 @@ modelRich2 <- lm(formula = richness ~ Dev_10,
             na.action = "na.fail",
             data = rich)
 
-AICc(modelRich, modelRich2)
+summary(modelRich)
 
-sjPlot::plot_model(modelRich, terms = "Dev_1", type = "pred")
-sjPlot::plot_model(modelRich2, terms = "Dev_10", type = "pred")
+AICc(modelRich, modelRich2)
+Weights(AICc(modelRich, modelRich2))
+
+
+a <- sjPlot::plot_model(modelRich, terms = "Dev_1", type = "pred", title = "")
+
+library(ggpubr)
+
+a +
+    geom_point(rich, mapping = aes(x = Dev_1, y = richness)) +
+    labs(x = "Proportion urbanization (1-km)", y = "Species richness") +
+    theme_classic()
 
 ## community weighted mean
 cwm <- cm_long_urbanization %>% 
@@ -138,8 +146,23 @@ cwm_urb <- left_join(cwm, urb)
 
 head(cwm_urb)
 
-ggplot(cwm_urb, mapping = aes(x = BodySize_cwm, y = Dev_10)) +
-    geom_point() +
-    geom_smooth(method = "lm")
-
 # CWM model
+modelCWM <- lm(formula = BodySize_cwm ~ Dev_1,
+                na.action = "na.fail",
+                data = cwm_urb)
+
+modelCWM2 <- lm(formula = BodySize_cwm ~ Dev_10,
+                 na.action = "na.fail",
+                 data = cwm_urb)
+
+AICc(modelCWM, modelCWM2)
+Weights(AICc(modelCWM, modelCWM2))
+
+summary(modelCWM2)
+
+b <- sjPlot::plot_model(modelCWM2, terms = "Dev_10", type = "pred", title = "")
+
+b + 
+    geom_point(cwm_urb, mapping = aes(x = Dev_10, y = BodySize_cwm)) +
+    labs(x = "Proportion urbanization (1-km)", y = "Body Size CWM") +
+    theme_classic()
