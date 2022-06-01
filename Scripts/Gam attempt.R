648859ce2c58a133c283a644ac8902ccf0e0fd40
Redfish303 <nfederico1@ufl.edu>
2022-06-01 11:57:41 -0400

    Further attempts at GAM discovering insufficient sample size for modelling.
    Updated light data sheet

--- Scripts/Gam attempt.R
+++ Scripts/Gam attempt.R
@@ -10,11 +10,11 @@ df_geo <- read.csv('data/cleanData.csv') %>%
                           true = doy + 365,
                           false = doy))
 
-tdf <- filter(df_geo, scientificName == 'Anomala innuba')
+tdf <- filter(df_geo, scientificName == 'Hydaticus bimarginatus')
               
 rist_test <- filter(tdf, Site == "Rist")
 
-gam_m <- gam(Count ~ s(doy2, k = 5), bs = "cr", data = rist_test)
+gam_m <- gam(Count ~ s(doy2, k = 15), bs = "cr", data = tdf)
 gam_p <- predict.gam(gam_m, newdata = data.frame(doy2 = 69:425),
                      type = "response", se = F)
 
@@ -23,4 +23,9 @@ gam_p_df <- data.frame(doy2 = 69:425, Count = gam_p)
 ggplot(gam_p_df, aes(x = doy2, y = Count)) +
     geom_smooth()
 
+ggplot(tdf, aes(x = doy2, y = Count)) + 
+    geom_point()
 
+gam.check(gam_m)
+
+##Result: Most species do not have enough data for us to do a GAM with.
