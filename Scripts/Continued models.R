a48061824e42c01e71243cd654ee60a4959b7516
Redfish303 <nfederico1@ufl.edu>
2022-06-29 11:25:05 -0400

    Continued model

---
+++ Scripts/Continued models.R
@@ -0,0 +1,51 @@
+library(dplyr)
+library(lme4)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+library(lmerTest)
+library(sjPlot)
+library(MuMIn)
+library(ggeffects)
+library(ggpubr)
+library(effects)
+library(rr2)
+library(stringr)
+
+#Script to run continued models
+
+cm <- read.csv("data/communitymatrixSite.csv")
+
+cm_long <- cm %>% 
+    pivot_longer(!X, names_to = "scientific_name", values_to = "abundance") %>% 
+    rename(Site = X) %>% 
+    mutate(scientific_name = str_replace(scientific_name,
+                                         pattern = "\\.", replacement = " "))
+
+#combine with urbanization data
+urb <- read.csv("data/impervious_surface.csv")
+cm_long_urbanization <- left_join(cm_long, urb)
+
+## combine with trait data
+traits <- read.csv("data/TraitData.csv")
+cm_long_urbanization <- left_join(cm_long_urbanization, traits, 
+                                  by = c("scientific_name" = "Species")) # normally distributed
+
+#combine with light data
+light <- read.csv("data/lightData.csv")
+cm_long_urbanization <- left_join(cm_long_urbanization, light)
+
+#Can't Join this and niche because of how we named the species in those
+range <- read.csv("data/geographicdata.csv")
+
+niche <- read.csv("data/nichedata.csv") %>% 
+cm_long_urbanization <- left_join(cm_long_urbanization, niche,
+                                  by = c("scientific_name" = "scientificName"))
+
+#matching species name for niche data
+species <- data.frame(unique(cm_long$scientific_name)) %>% 
+    rename(scientificName = unique.cm_long.scientific_name.)
+    left_join(species, niche,
+              by = c("ID" = "X"))
+           
+    
